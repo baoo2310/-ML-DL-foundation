3eea900c4dc2ee2b686240df9c6fa9b2e66cea42
baoo2310 <baoo231005@gmail.com>
2025-08-29 14:14:42 +0700

    Model Evaluation Techniques

---
+++ feature_engineering/model_evaluation_2.py
@@ -0,0 +1,20 @@
+from sklearn.datasets import fetch_california_housing
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
+
+data = fetch_california_housing()
+X, y = data.data, data.target
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+model = LinearRegression()
+model.fit(X_train, y_train)
+y_pred = model.predict(X_test)
+mae = mean_absolute_error(y_test, y_pred)
+mse = mean_squared_error(y_test, y_pred)
+r2 = r2_score(y_test, y_pred)
+
+print(f"Mean Absolute Error (MAE): {mae:.2f} ")
+print(f"Mean Square Error (MSE): {mse:.2f} ")
+print(f"R2 Score (r2): {r2:.2f} ")
